8e4c2365f39fe48d42496c6cf4d026a0262de4d5
Diego Paz <diegopaz25814@gmail.com>
2025-05-05 01:06:42 -0500

    Cambio de servicio de RNN

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  API: 'https://p20241053.up.railway.app/api',
+  API: 'https://20241053-production.up.railway.app/api',
+  API_RNN: 'https://20241053rnn-production.up.railway.app/consultar/',
   environmentName: 'QA',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   API: 'https://20241053-production.up.railway.app/api',
-  API_RNN: 'https://redes.up.railway.app/consultar/',
+  API_RNN: 'https://20241053rnn-production.up.railway.app/consultar/',
   environmentName: 'Dev-Local',
 
 };
